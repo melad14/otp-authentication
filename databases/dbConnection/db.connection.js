972b4408d7afa28345a0236372c2a9d8ff6f498c
Melad shehata <117023904+melad14@users.noreply.github.com>
2024-05-21 01:47:03 +0300

    Update db.connection.js

--- databases/dbConnection/db.connection.js
+++ databases/dbConnection/db.connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export function conn(){
-    mongoose.connect("mongodb://localhost:27017/userAPI").then(()=>{
+    mongoose.connect("mongodb+srv://milad:SK3d3QWXseAZgR0x@cluster0.lhwsef7.mongodb.net/user-auth").then(()=>{
         console.log("connected......");
     }).catch((err)=>{
         console.log("error......",err);
